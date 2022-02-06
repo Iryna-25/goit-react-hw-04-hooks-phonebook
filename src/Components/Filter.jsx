e8a770a0666948c4ef7e02f881a54f80c13d1d3c
Iryna Shandar <iryna.shandar@gmail.com>
2022-02-06 17:08:08 +0200

    update

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import { FilterTitle } from "./Filter.styled";
-
-// const Filter = ({ value, onChange }) => {
-//   return (
-//     <div>
-//       <FilterTitle>Find contacts by name</FilterTitle>
-//       <input type="text" value={value} onChange={onChange}></input>
-//     </div>
-//   );
-// };
-
-// export default Filter;
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Header2, SearchInput } from './Filter.styled';
