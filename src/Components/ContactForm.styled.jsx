70288435289b418462978f290bcef364ff7c8d85
Iryna Shandar <iryna.shandar@gmail.com>
2022-02-06 17:01:45 +0200

    components

---
+++ src/Components/ContactForm.styled.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+export const Header = styled.h1`
+  font-style: italic;
+  font-size: 30px;
+`;
+
+export const Form = styled.form`
+  border: 2px solid #000;
+  border-radius: 7px;
+  padding: 20px 20px;
+  margin-bottom: 50px;
+  label {
+    display: block;
+    margin-bottom: 20px;
+    font-style: italic;
+  }
+`;
+
+export const Input = styled.input`
+  border: 2px solid #000;
+  border-radius: 7px;
+  width: 50%;
+  height: 25px;
+  display: block;
+  padding-right: 10px;
+  padding-left: 10px;
+`;
+
+export const Button = styled.button`
+  border: 2px solid #000;
+  border-radius: 7px;
+  padding: 7px;
+  background-color: greenyellow;
+  &:hover {
+    box-shadow: 0px 2px 1px 1px rgba(0, 0, 0, 0.15);
+    background-color: green;
+    cursor: pointer;
+  }
+`;
--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -21,3 +21,38 @@
 // };
 
 // export default Contacts;
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List, Item, DeleteButton } from './ContactsList.styled';
+
+const ContactList = ({ contacts, onRemoveContact }) => (
+  <List>
+    {contacts.map(contact => (
+      <Item key={contact.id}>
+        {contact.name + ' : ' + contact.number}
+        {
+          <DeleteButton
+            type="button"
+            name="delte"
+            onClick={() => onRemoveContact(contact.id)}
+          >
+            Delete
+          </DeleteButton>
+        }
+      </Item>
+    ))}
+  </List>
+);
+
+ContactList.propTypes = {
+  onRemoveContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+};
+export default ContactList;
